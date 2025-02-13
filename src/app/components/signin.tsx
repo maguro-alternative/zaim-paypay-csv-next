caf0183cdd5fe6429e752a7d3305b2dbae17ca71
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-13 09:57:55 +0900

    signinコンポーネント作成

--- src/app/components/signin.tsx
+++ src/app/components/signin.tsx
@@ -1,11 +1,37 @@
 'use client';
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession, SessionProvider } from "next-auth/react";
 
 export default function SignIn() {
   return (
+    <SessionProvider>
+      <SignInContent />
+    </SessionProvider>
+  );
+}
+
+function SignInContent() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Signed in as {session?.user?.name}</p>
+        <button
+          className="p-2 bg-red-500 text-white rounded-md"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      </div>
+    );
+  }
+
+  return (
     <button
       className="p-2 bg-blue-500 text-white rounded-md"
-      onClick={() => signIn()}
-    />
+      onClick={() => signIn("zaim", { callbackUrl: "http://localhost:3000" })}
+    >
+      Sign in with Zaim
+    </button>
   );
 }
