16eca6a9f182e9074e03ad97248bde1be965ac5a
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-14 22:26:29 +0900

    ユニコｫｫｫン！！！！！！！

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,6 +1,44 @@
 import type { NextAuthOptions } from "next-auth";
 
+declare module "next-auth" {
+  interface User {
+    id: string | number;
+    name: string;
+    image: string;
+    accessToken?: string;
+    accessTokenSecret?: string;
+  }
+  interface AdapterUser {
+    id: string | number;
+    name: string;
+    image: string;
+    accessToken?: string;
+    accessTokenSecret?: string;
+  }
+
+  interface Session {
+    user: User;
+  }
+}
+declare module "next-auth/jwt" {
+  interface JWT {
+    name?: string | null
+    email?: string | null
+    picture?: string | null
+    sub?: string
+    accessToken: string | undefined;
+    accessTokenSecret: string | undefined;
+  }
+}
+
 const nextAuthOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET
+  },
   providers: [
     {
       id: "zaim",
@@ -11,12 +49,15 @@ const nextAuthOptions: NextAuthOptions = {
       authorization: `https://auth.zaim.net/users/auth`,
       accessTokenUrl: `https://api.zaim.net/v2/auth/access`,
       profileUrl: `https://api.zaim.net/v2/home/user/verify`,
-      async profile(profile, credentials) {
+      async profile(profile, tokens) {
         return {
           id: profile.me.id,
           name: profile.me.name,
           image: profile.me.profile_image_url,
         };
+      },
+      token: {
+        url: `https://api.zaim.net/v2/auth/access`,
       },
       clientId: process.env.ZAIM_CLIENT_ID,
       clientSecret: process.env.ZAIM_CLIENT_SECRET,
@@ -24,16 +65,34 @@ const nextAuthOptions: NextAuthOptions = {
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
+      user.accessToken = account?.access_token;
+      user.accessTokenSecret = account?.access_token_secret as string | undefined;
       return true
     },
     async redirect({ url, baseUrl }) {
       return url
     },
-    async session({ session, user, token }) {
-      return session
+    async jwt({ token, user, account, profile }) {
+      if (account) {
+        token.accessToken = account.oauth_token as string | undefined;
+        token.accessTokenSecret = account.oauth_token_secret as string | undefined;
+      }
+      return token;
     },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      return token
+    async session({ session, token, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          accessToken: token.accessToken,
+          accessTokenSecret: token.accessTokenSecret,
+        },
+      }
+    },
+  },
+  events: {
+    session(message) {
+      // console.log('session message', message)
     },
   },
   debug: process.env.NODE_ENV === 'development',
