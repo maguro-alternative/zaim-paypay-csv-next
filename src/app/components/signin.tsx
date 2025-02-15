41e0cfd3e51bdac8fb248eb4b7f3ae4ff7a2289a
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-15 11:36:57 +0900

    signinとsignoutコンポーネント追加

--- src/app/components/signin.tsx
+++ src/app/components/signin.tsx
@@ -1,31 +1,7 @@
 'use client';
-import { signIn, signOut, useSession, SessionProvider } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
 export default function SignIn() {
-  return (
-    <SessionProvider>
-      <SignInContent />
-    </SessionProvider>
-  );
-}
-
-function SignInContent() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <div>
-        <p>Signed in as {session?.user?.name}</p>
-        <button
-          className="p-2 bg-red-500 text-white rounded-md"
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </div>
-    );
-  }
-
   return (
     <button
       className="p-2 bg-blue-500 text-white rounded-md"
---
+++ src/app/components/signout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { signOut } from "next-auth/react";
+import type { Session } from "next-auth";
+
+interface SignOutProps {
+  session: Session;
+}
+
+export default function SignOut({ session }: SignOutProps) {
+  return (
+    <div>
+      <p>Signed in as {session?.user?.name}</p>
+      <button
+        className="p-2 bg-red-500 text-white rounded-md"
+        onClick={() => signOut()}
+      >
+        Sign out
+      </button>
+    </div>
+  );
+}
