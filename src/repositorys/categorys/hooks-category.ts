063284b81bb8b8a130663c3448b7c0a00ebcdf08
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-16 21:47:23 +0900

    any許容

--- src/repositorys/categorys/hooks-category.ts
+++ src/repositorys/categorys/hooks-category.ts
@@ -9,6 +9,7 @@ export async function getCategorysData(session: Session | null) {
       "https://api.zaim.net/v2/home/category",
       session?.user?.accessToken as string,
       session?.user?.accessTokenSecret as string,
+      /* eslint-disable @typescript-eslint/no-explicit-any */
       (err: any, data: any) => {
         if (err) {
           reject(err);
