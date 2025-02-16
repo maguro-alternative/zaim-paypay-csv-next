12ca38da08a11aa216e743a70504c11ff9391fe8
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-16 21:27:46 +0900

    page更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-import SignIn from "./components/signin";
-import CSVUploader from "./components/CSVUploader";
+import { getServerSession } from "next-auth/next";
 
-export default function Home() {
+import SignIn from "./components/signin";
+import SignOut from "./components/signout";
+import CSVUploader from "./components/CSVUploader";
+import nextAuthOptions from "../app/api/auth/[...nextauth]";
+
+export default async function Home() {
+  const session = await getServerSession(nextAuthOptions);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold">Next.js + NextAuth.js</h1>
       <p className="text-lg text-center">
         Example integration of Next.js with NextAuth.js
       </p>
-      <div className="flex flex-col gap-4"> 
+      {session ? (
+        <>
+          <SignOut session={session} />
+          <CSVUploader />
+        </>
+      ) : (
         <SignIn />
-        <CSVUploader />
-      </div>
+      )}
     </div>
   );
 }
