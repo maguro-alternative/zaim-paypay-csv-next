b4343e2c0591452aa9503f87679c8af996f3f4fb
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-16 14:58:35 +0900

    oauthに変更

--- src/repositorys/oauth.ts
+++ src/repositorys/oauth.ts
@@ -1,13 +1,15 @@
-import OAuth from "oauth-1.0a";
-import crypto from "crypto";
+import { promisify } from "util";
 
-export const ZaimOAuth = new OAuth({
-  consumer: {
-    key: process.env.ZAIM_CLIENT_ID as string,
-    secret: process.env.ZAIM_CLIENT_SECRET as string,
-  },
-  signature_method: "HMAC-SHA1",
-  hash_function(base_string, key) {
-    return crypto.createHmac("sha1", key).update(base_string).digest("base64");
-  },
-});
+import oauth from "oauth";
+
+export const ZaimOAuth = new oauth.OAuth(
+  "https://api.zaim.net/v2/auth/request",
+  "https://api.zaim.net/v2/auth/access",
+  process.env.ZAIM_CLIENT_ID as string,
+  process.env.ZAIM_CLIENT_SECRET as string,
+  "1.0",
+  null,
+  "HMAC-SHA1"
+);
+
+export const ZaimGetAsync = promisify(ZaimOAuth.get).bind(ZaimOAuth);
