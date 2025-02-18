68bb876f4f5214dcfc5bb550ad5e5c4ceb0c8c14
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-18 09:25:39 +0900

    accountのapi追加

---
+++ src/repositorys/accounts/hooks-accounts.ts
@@ -0,0 +1,25 @@
+import { ZaimOAuth } from "../oauth";
+import type { Session } from "next-auth";
+
+import { accountResponse } from "./types";
+
+export async function getAccountsData(session: Session | null) {
+  const accountAsync: Promise<accountResponse> = new Promise((resolve, reject) => {
+    ZaimOAuth.get(
+      "https://api.zaim.net/v2/home/account",
+      session?.user?.accessToken as string,
+      session?.user?.accessTokenSecret as string,
+      (err: any, data: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          // 文字列として返ってくるデータをJSONとしてパースし、型を適用
+          const parsedData = JSON.parse(data) as accountResponse;
+          resolve(parsedData);
+        }
+      }
+    );
+  });
+
+  return accountAsync;
+};
