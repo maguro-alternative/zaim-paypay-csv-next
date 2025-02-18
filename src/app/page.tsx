7062e806a0b4f3ec00e37d2e8b7c40e095f53573
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-19 08:37:37 +0900

    文面修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-teal-200">
       <h1 className="text-4xl font-bold">Zaim PayPay連携</h1>
       <p className="text-lg text-center">
-        ZaimとPayPayを連携して、支出データを自動で取得します。
+        Zaim APIでPayPayを連携して、支出データを自動で取得します。
       </p>
       {session ? (
         <>
