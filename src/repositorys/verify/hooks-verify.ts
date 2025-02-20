354fd5ed62a00c40531584808661fcbbcd27bdb9
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-20 13:59:23 +0900

    try

--- src/repositorys/verify/hooks-verify.ts
+++ src/repositorys/verify/hooks-verify.ts
@@ -3,23 +3,27 @@ import type { Session } from "next-auth";
 import { VerifyResponse } from "./types";
 
 export async function getVerifyData(session: Session | null): Promise<VerifyResponse> {
-  const verifyAsync: Promise<VerifyResponse> = new Promise((resolve, reject) => {
-    ZaimOAuth.get(
-      "https://api.zaim.net/v2/home/user/verify",
-      session?.user?.accessToken as string,
-      session?.user?.accessTokenSecret as string,
-      /* eslint-disable @typescript-eslint/no-explicit-any */
-      (err: any, data: any) => {
-        if (err) {
-          console.error("Failed to get verify data:", err);
-          reject(err);
-        } else {
-          const parsedData = JSON.parse(data) as VerifyResponse;
-          resolve(parsedData);
+  try {
+    const verifyAsync: Promise<VerifyResponse> = new Promise((resolve, reject) => {
+      ZaimOAuth.get(
+        "https://api.zaim.net/v2/home/user/verify",
+        session?.user?.accessToken as string,
+        session?.user?.accessTokenSecret as string,
+        /* eslint-disable @typescript-eslint/no-explicit-any */
+        (err: any, data: any) => {
+          if (err) {
+            console.error("Failed to get verify data:", err);
+            reject(err);
+          } else {
+            const parsedData = JSON.parse(data) as VerifyResponse;
+            resolve(parsedData);
+          }
         }
-      }
-    );
-  });
-
-  return verifyAsync;
+      );
+    });
+    return verifyAsync;
+  } catch (error) {
+    console.error("Failed to get verify data:", error);
+    throw error;
+  }
 };
