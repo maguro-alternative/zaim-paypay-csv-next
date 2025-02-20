6ecf1b71d33034ce118dae33ff5adb7bdb26b005
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-02-20 13:54:07 +0900

    try

---
+++ src/repositorys/money/hooks-money.ts
@@ -0,0 +1,35 @@
+import { ZaimOAuth } from "../oauth";
+import type { Session } from "next-auth";
+
+import { MoneyParams, MoneyResponse } from "./types";
+
+import { stringify } from 'querystring';
+
+export async function getMoneysData(
+  session: Session | null,
+  params?: MoneyParams
+): Promise<MoneyResponse>  {
+  let url = "https://api.zaim.net/v2/home/money";
+
+  if (params) {
+    const queryString = stringify(params);
+    url += `?${queryString}`;
+  }
+  const moneyAsync: Promise<MoneyResponse> = new Promise((resolve, reject) => {
+    ZaimOAuth.get(
+      url,
+      session?.user?.accessToken as string,
+      session?.user?.accessTokenSecret as string,
+      (err: any, data: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          const parsedData = JSON.parse(data) as MoneyResponse;
+          resolve(parsedData);
+        }
+      }
+    );
+  });
+
+  return moneyAsync;
+};
